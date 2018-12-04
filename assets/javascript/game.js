e7d43a33a1f518abc6d630707e91374a30def28a
Kelsey Tobin <etobinkelsey@gmail.com>
2018-12-04 14:22:12 -0700

    js update

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+//Global Variables ///////////////////////////////////////////////////////////
+
+//create an array that lists out all of the options
+var words = ["sequoia", "glacier", "olympic", "redwood", "saguaro"];
+
+var lettersAvailable = "abcdefghijklmnopqrstuvwxyz";
+var lettersMatched = ;
+
+var currentWord = "" ;
+var messages = "" ;
+var userGuess = "" ;
+
+//create variables that hold number of wins, losses, and guesses left
+var chances = 7;
+var wins = 0;
+var losses = 0;
+
+//create variables that hold references to the places in HTML where we want to display things
+var lettersMatchedText = document.getElementById("letters-matched-text");
+var wrongGuessesText = document.getElementById("wrong-guesses-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementsById("losses-text");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions///////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Execution////////////////////////////////////////////////////////////////////
